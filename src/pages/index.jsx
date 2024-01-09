b4cef9433cc0da0f221dae01671f6185cbc6850a
Safarqwerty <safar.alam79@gmail.com>
2024-01-10 01:29:07 +0800

    Fitur Filter Data dan Pengenalan useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article";
 
 function Homepage() {
+    const [search, setSearch] = useState("");
+    const changeSearch = (event) => {
+        setSearch(event.target.value)
+    };
+
     return (
         <>
             <h1>Simple Blog</h1>
-            {posts.map((blog) => (
-                <Article 
-                    key={blog.id}
-                    title={blog.title}
-                    tags={blog.tags}
-                    date={blog.date} />
+            <div>
+                Cari Artikel : <input onChange={changeSearch}></input>
+            </div>
+            <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+            {posts.map(({ title, tags, date }, index) => (
+            <Article {...{ index, title, tags, date }} key={index} />
             ))}
         </>
     );
