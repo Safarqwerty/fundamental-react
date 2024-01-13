d4e1270c53f1cbf7a1ad9b2148987b1e798a457a
Safarqwerty <safar.alam79@gmail.com>
2024-01-13 18:52:55 +0800

    Error Page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -5,6 +5,7 @@ import About from "../pages/About";
 import Blog from "../pages/blogs";
 import Post from "../pages/blogs/_id";
 
+import ErrorPage from "../components/ErrorPage";
 import { posts, postById } from "../apis/loaders";
 
 
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
