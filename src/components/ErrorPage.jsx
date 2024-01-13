e4851267800cccfbfe0ad950272c10f8d33fd4e5
Safarqwerty <safar.alam79@gmail.com>
2024-01-13 19:30:31 +0800

    Dynamic Style untuk NavLink

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { useRouteError } from "react-router-dom";
 
 function ErrorPage () {
     const error = useRouteError();
 
-    return <div>Error Gess!!!</div>
+    return <div>Error Gess!!!{error}</div>
 }
 
 export default ErrorPage;
