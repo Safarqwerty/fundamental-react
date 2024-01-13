9aa877c1f31f719c13f4bc98eeb719ab9105c114
Safarqwerty <safar.alam79@gmail.com>
2024-01-13 13:47:55 +0800

    State Management dengan useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Homepage from './pages/Index';
+import { GlobalContext } from './context';
 
 function App() {
+  const user = {
+    username: 'safarqwerty'
+  }
+
   return (
     <div className='App'>
-      <Homepage />
+      <GlobalContext.Provider value={user}>
+        <Homepage />
+      </GlobalContext.Provider>
     </div>
   )
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,17 +1,21 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 // Import library prop-types untuk melakukan validasi tipe data properti
 import PropTypes from 'prop-types';
-
+import { useContext } from 'react';
+import { GlobalContext } from '../context';
 // Komponen Article menerima props yang berisi title, date, dan tags
 const ArticleStatus = ({isNew}) => {
   return isNew && <span>- Baru</span>
 }
 
-const NewArticle = () => {
-  return <span>- (Baru)</span>
-}
+// const NewArticle = () => {
+//   return <span>- (Baru)</span>
+// }
 
 function Article(props) {
+  const user = useContext(GlobalContext)
+
     return (
         <>
         {/* Menampilkan judul artikel */}
@@ -22,8 +26,11 @@ function Article(props) {
             Date: {props.date}, tags: {props.tags.join(", ")} 
             {/* {props.isNew && "- Baru!"} */}
             <ArticleStatus isNew={props.isNew} />
-            {props.isNew && <NewArticle />}
+            {/* {props.isNew && <NewArticle />} */}
         </small>
+        <div>
+          <small>Ditulis oleh {user.username}</small>
+        </div>
         </>
     );
 }
